41c5b9f08a6b6de8234a538705c9dc5fcf3fa2de
Alicia Santiago <albrown07@mac.com>
2019-02-28 22:08:48 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -2,8 +2,9 @@ require_relative "./user.rb"
 class Teacher < User
 
 def teach(array)
+  array = KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
   array.sample(n, random: rng)
-  KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
+
 end
 
 end

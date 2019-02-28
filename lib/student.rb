59bddb04f100328df1ee9ecbdc750120c63bc0e7
Alicia Santiago <albrown07@mac.com>
2019-02-28 22:24:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,15 +3,12 @@ class Student < User
 
 attr_accessor :knowledge
 
-def initialize
-  @knowledge = []
-end
+  def initialize
+    @knowledge = []
+  end
 
-def learn(string)
-  @knowledge << string
-end
+  def learn(string)
+    @knowledge << string
+  end
 
-# def knowledge
-#   @knowledge
-# end
 end
